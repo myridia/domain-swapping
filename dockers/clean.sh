676efe6f4321672549a6debbaa07562a867d7bee
veto <veto@lab.local>
2025-04-26 08:26:25 +0700

    update

--- dockers/clean.sh
+++ dockers/clean.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
 docker rm --force `docker ps -qa`
-#docker rmi --force `docker images -aq`
 docker volume prune -f
+
+if [ "$1" == "all" ] || [ $# -gt 1 ]; then
+  echo "clean all including images"
+  docker rmi --force `docker images -aq`    
+fi
+
